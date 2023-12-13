379be1b50c50b5979fd445bcf3d82efcdd520691
lollo03 <03lollo@gmail.com>
2023-12-13 17:18:14 +0100

    Added new stuff...

---
+++ toMySQL/server/index.js
@@ -0,0 +1,76 @@
+// 1 DATABASE AIRQUALITY
+// 1 TABELLA (data):
+// |Data rivelazione|Dati...|DEVICEID|
+// 2 TABELLA (devices):
+// |deviceID|FRIENDLYNAME|LASTIPING|token|
+
+const express = require('express')
+const mariadb = require('mariadb');
+var bodyParser = require('body-parser')
+var cors = require('cors');
+require("dotenv").config();
+
+const app = express()
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+app.use(cors())
+
+const port = 80
+
+const pool = mariadb.createPool({
+    host: process.env.mariadb_host,
+    user: process.env.mariadb_user,
+    password: process.env.mariadb_password,
+    database: process.env.mariadb_database,
+    connectionLimit: 5
+});
+
+async function connectDB() {
+    return await pool.getConnection()
+}
+
+async function checkToken(token) {
+    conn = pool
+    let res = await conn.query("SELECT token FROM `devices`")
+    let tokenCache = res.map(function (res) {
+        return res['token'];
+    });
+    if (tokenCache.includes(token)) {
+        return 1
+    }
+    return 0
+}
+
+
+app.post('/new', async (req, res) => {
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+    console.log(`Ricevuto /new da ${ip}`)
+    if (!await checkToken(req.headers["token"]) || !req.headers["token"]) { //il token non è nel db
+        console.log("Access token non valido!")
+        res.sendStatus(401)
+        return
+    }
+    console.log("Token riconosciuto, scrivo nel database")
+    if (!req.body["PM25"] || !req.body["deviceID"]) {
+        console.log("La richiesta non ha i parametri body necessari!")
+        res.sendStatus(400)
+        return
+    }
+    conn = pool
+    try { //un bel catch-all
+        await conn.query(`INSERT INTO data (dateTime, PM25, deviceID) VALUE(NOW(), ${req.body["PM25"]}, ${req.body["deviceID"]});`)
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500)
+        return
+    }
+    console.log('Scrittura completata...')
+    res.sendStatus(200)
+})
+
+app.listen(port, async () => {
+    await connectDB()
+    console.log(`App avviata su ${port}, database connesso!`)
+})
---
+++ toMySQL/webClient/webClient.ino
@@ -0,0 +1,115 @@
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include "TickTwo.h"
+
+// WIFI settings
+const char* ssid = "";
+const char* password = "";
+IPAddress local_IP(192, 168, 100, 69);
+IPAddress gateway(192, 168, 100, 1);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress primaryDNS(1, 1, 1, 1);  
+
+// PIN SETTINGS
+SoftwareSerial sensorSerial(2, 4); //4 NOT USED
+
+// Remote server settings
+const char* serverName = "http://192.168.100.88/new";
+const char* accessToken = "";
+const char* deviceID = "1";
+
+// Global vars
+uint8_t serialRxBuf[255];
+uint8_t rxBufIdx = 0;
+int sum = 0;
+int sumCounter = 0;
+HTTPClient http; // HTTP CLIENT
+WiFiClient client;
+
+void senData();
+
+// Timer
+TickTwo timer(senData, 1000*60); // Every minute...
+
+void senData() { //perdiocally sends post request
+  Serial.println("Trying to send data...");
+  if(WiFi.status()!= WL_CONNECTED){
+    return; // exit if wifi is not connected
+  }
+  http.begin(client, serverName);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  http.addHeader("token", accessToken);
+  if(sumCounter == 0){ //if there isn't new data exit the fuction
+    Serial.println("No data avaible... skipping");
+    return;
+  }
+  float temp = sum/sumCounter;
+  String httpRequestData = "deviceID=" + String(deviceID) + "&PM25=" + String(temp);
+  sum = 0;
+  sumCounter = 0;
+  int httpResponseCode = http.POST(httpRequestData); 
+  Serial.print("HTTP Response code: ");
+  Serial.println(httpResponseCode);
+  http.end(); // Free resources
+}
+
+
+void setup() {
+  // Configures static IP address
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS)) {
+    Serial.println("STA Failed to configure");
+  }
+
+  WiFi.begin(ssid, password);
+  sensorSerial.begin(9600);
+  Serial.begin(9600);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  timer.start();
+  senData();
+}
+
+void loop() {
+  timer.update();
+  if (sensorSerial.available()) {
+        while (sensorSerial.available()) {
+            
+            int temp = sensorSerial.read();
+            serialRxBuf[rxBufIdx++] = temp;
+            Serial.print(temp, HEX);
+            Serial.print(" ");
+
+            delay(15); //for some reasons this delay is needed
+
+            if (rxBufIdx >= 64) {
+                memset(serialRxBuf, 0, sizeof(serialRxBuf));
+                rxBufIdx = 0;
+            }
+        }
+        
+        rxBufIdx = 0;
+        bool headerValid = serialRxBuf[0] == 0x16 && serialRxBuf[1] == 0x11 && serialRxBuf[2] == 0x0B;
+            if(headerValid){
+              
+              int pm25 = (serialRxBuf[5] << 8) | serialRxBuf[6];
+              Serial.print(" decoded: ");
+              Serial.println(pm25);
+              memset(serialRxBuf, 0, sizeof(serialRxBuf));
+              rxBufIdx = 0;
+              sum = sum +  pm25;
+              sumCounter++;
+        }
+        
+   }
+}
